96022b9b0cd2e2b549d93678506d367b754f9848
ayse- <ayse-@DESKTOP-2S5TDQT>
2024-05-03 23:14:11 +0300

    aaaa

---
+++ Core/CarBookProject.Application/Features/Mediator/Commands/ServiceCommand/RemoveServiceCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Commands.ServiceCommand
+{
+    public class RemoveServiceCommand:IRequest
+    {
+        public RemoveServiceCommand(int ıd)
+        {
+            Id = ıd;
+        }
+
+        public int Id { get; set; } 
+    }
+}
---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/ServiceHandlers/GetServiceByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using CarBookProject.Application.Features.Mediator.Queries.ServiceQueries;
+using CarBookProject.Application.Features.Mediator.Results.ServiceResult;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.ServiceHandlers
+{
+    public class GetServiceByIdQueryHandler : IRequestHandler<GetServiceByIdQuery, GetServiceByIdQueryResult>
+    {
+        private readonly IRepository<Service> _repository;
+
+        public GetServiceByIdQueryHandler(IRepository<Service> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetServiceByIdQueryResult> Handle(GetServiceByIdQuery request, CancellationToken cancellationToken)
+        {
+           var value = await _repository.GetByIdAsync(request.Id);
+            return new GetServiceByIdQueryResult
+            {
+                ServiceID = value.ServiceID,
+                Description = value.Description,
+                IconUrl = value.IconUrl,
+                Title = value.Title,
+            };
+        }
+    }
+}
