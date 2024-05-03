c81103f0f4659af14574649c1858b47498410db1
ayse- <ayse-@DESKTOP-2S5TDQT>
2024-05-03 20:35:28 +0300

    aaaa

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/LocationHandlers/UpdateLocationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.LocationCommand;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.LocationHandlers
+{
+    public class UpdateLocationCommandHandler : IRequestHandler<UpdateLocationCommand>
+    {
+        private readonly IRepository<Location> _repository;
+
+        public UpdateLocationCommandHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateLocationCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.LocationID);
+            values.LocationID = request.LocationID;
+            values.LocationName = request.LocationName;
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
---
+++ Presentations/CarBookProject.WebApi/Controllers/LocationsController.cs
@@ -0,0 +1,55 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.LocationCommand;
+using CarBookProject.Application.Features.Mediator.Queries.LocationQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LocationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LocationList()
+        {
+            var values = await _mediator.Send(new GetLocationQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLocation(int id)
+        {
+            var value = await _mediator.Send(new GetLocationByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLocation(CreateLocationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Konum eklendi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateLocation(UpdateLocationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Konum güncellendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveLocation(int id)
+        {
+            await _mediator.Send(new RemoveLocationCommand(id));
+            return Ok("Konum silindi");
+        }
+    }
+}
