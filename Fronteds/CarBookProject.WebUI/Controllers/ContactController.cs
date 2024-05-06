64e5bfbf087c4a10fb76f953ea18a6bb94796f75
ayse- <ayse-@DESKTOP-2S5TDQT>
2024-05-06 18:48:11 +0300

    aaa

---
+++ Fronteds/CarBookProject.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+          
+            return View();
+        }
+    }
+}
