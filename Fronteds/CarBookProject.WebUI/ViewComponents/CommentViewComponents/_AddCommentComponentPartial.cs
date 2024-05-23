a3fb52240aefc21c732f003f10f2b396badae3e1
ayse- <ayse-@DESKTOP-2S5TDQT>
2024-05-23 13:03:56 +0300

    aaa

---
+++ Fronteds/CarBookProject.WebUI/ViewComponents/CommentViewComponents/_AddCommentComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.ViewComponents.CommentViewComponents
+{
+    public class _AddCommentComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
